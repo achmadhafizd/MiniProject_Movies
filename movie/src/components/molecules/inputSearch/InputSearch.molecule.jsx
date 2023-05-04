64fc41b9b1d8345eab66e6f331ee2effac03813a
achmadhafizd <ahmadhafizd2104@gmail.com>
2023-05-04 09:48:11 +0700

    feat(Search) : Add Search Input

--- movie/src/components/molecules/inputSearch/InputSearch.molecule.jsx
+++ movie/src/components/molecules/inputSearch/InputSearch.molecule.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SearchInput = (props) => {
+function SearchInput({ onChange }) {
   return (
     <div>
       <form className="flex items-center">
@@ -11,7 +11,7 @@ const SearchInput = (props) => {
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <svg
               aria-hidden="true"
-              className="w-5 h-5 text-gray-500 dark:text-gray-400"
+              className="w-5 h-5 text-green-700 dark:text-pink-500"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -20,20 +20,20 @@ const SearchInput = (props) => {
                 fillRule="evenodd"
                 d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
                 clipRule="evenodd"
-              ></path>
+              />
             </svg>
           </div>
           <input
             type="text"
             id="simple-search"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-lime-100 font-medium placeholder-green-700 placeholder-opacity-50 border border-lime-100 text-green-700 text-sm rounded-lg focus:ring-emerald-500 focus:border-emerald-500 block w-full pl-10 p-2.5  dark:bg-indigo-300 dark:border-pink-600 dark:placeholder-pink-500 dark:placeholder-opacity-50 dark:text-pink-500 dark:font-medium dark:focus:ring-pink-500 dark:focus:border-pink-500"
             placeholder="Search movie..."
-            onChange={props.onChange}
+            onChange={onChange}
           />
         </div>
       </form>
     </div>
   );
-};
+}
 
 export default SearchInput;
