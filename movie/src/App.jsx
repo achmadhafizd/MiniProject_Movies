4ede771b6a8be83c57c0e57ee9072c5a38753cda
achmadhafizd <ahmadhafizd2104@gmail.com>
2023-05-04 09:56:23 +0700

    Fix(Dev) :  Mod App, vite, package json

--- movie/src/App.jsx
+++ movie/src/App.jsx
@@ -1,15 +1,34 @@
 import React from "react";
-import LandingPage from "./views/LandingPage/LandingPage.view";
 import { Provider } from "react-redux";
-import store from "./config/redux/store";
+import { ApolloProvider } from "@apollo/client";
+import { ToastContainer } from "react-toastify";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persiststore } from "./config/redux/store";
+import RootRouter from "./RootRouter";
+import client from "./config/Apollo";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="dark">
-        <LandingPage />
-      </div>
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persiststore}>
+          <ToastContainer
+            position="top-center"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
+          <RootRouter />
+        </PersistGate>
+      </Provider>
+    </ApolloProvider>
   );
 }
 
