c132d799f04874f925d6f30f56734f08885a4246
achmadhafizd <ahmadhafizd2104@gmail.com>
2023-05-04 10:46:52 +0700

    Fix(Template) : Mod Landing Template

--- movie/src/components/templates/LandingPage/LandingPage.template.jsx
+++ movie/src/components/templates/LandingPage/LandingPage.template.jsx
@@ -1,12 +1,27 @@
 import * as React from "react";
 import { Outlet } from "react-router-dom";
 import { Footer, Navibar } from "../../organism";
+import { Carousels } from "../../molecules";
+import { usePopularMoviesSelector } from "../../../config/redux/movies/moviesSelector";
 
 function LandingPageTemplate() {
+  const popularMovieList = usePopularMoviesSelector();
+  const baseImgUrl = import.meta.env.VITE_BASEIMGURL;
+
   return (
     <div>
       <Navibar />
       <div className="bg-lime-50 dark:bg-indigo-950">
+        <Carousels
+          isiCarousels={popularMovieList
+            .filter((movie) => movie.poster_path !== null)
+            .map((movie) => (
+              <img
+                src={`${baseImgUrl}/${movie.poster_path}`}
+                alt={movie.title}
+              />
+            ))}
+        />
         <Outlet />
       </div>
       <Footer />
