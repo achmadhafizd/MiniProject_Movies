c839a94e9de5e9bc8f0bbcad38f05787070886bd
achmadhafizd <ahmadhafizd2104@gmail.com>
2023-05-07 00:57:32 +0700

    Fix(Template) : Carousels

--- movie/src/components/molecules/index.js
+++ movie/src/components/molecules/index.js
@@ -1,5 +1,3 @@
 export { default as SearchInput } from "./inputSearch/InputSearch.molecule";
 export { default as RatingStars } from "./RatingStars/RatingStars.molecule";
-export { default as Carousels } from "./Carousels/Carousels.molecule";
-export { default as Comments } from "./Comments/comments.molecule";
 export { default as EditDeleteButton } from "./EditDeleteButton/EditDeleteButton.molecule";
